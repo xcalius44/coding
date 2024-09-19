2c258596754a3f6cc3d3ba0651972f49db3ea185
Artem <xcalius25@gmail.com>
2024-09-19 18:18:21 +0300

    Update

---
+++ java_script/script.js
@@ -0,0 +1,126 @@
+
+// let a = 10;
+// console.log(a);
+// a = 20;
+
+// const b = 15;
+// console.log(b);
+
+// typeof
+// console.log(typeof b);
+
+// const value = "3";
+// console.log(Number(value));
+// console.log(1 + 2);
+// console.log(Number.isNaN(value));    // false
+
+// Number.parseInt()
+// consol.log(Number.parseInt("jkjvje"))    // NaN
+// consol.log(Number.parseInt("94fifdhvhv"))    // 94
+// consol.log(Number.parseInt("nvejknv8.9jkendjk")) // 8
+
+// Number.parseFloat()
+// consol.log(Number.parseFloat("jevk"))  // NaN
+// consol.log(Number.parseFloat("nkj45n"))    // 45
+// consol.log(Number.parseFSloat("njg5.6vbek"))    // 5.6
+
+// console.log(0.1 + 0.2 === 0.3);  // false
+// console.log(0.1 + 0.2);  // 0.30000000000000004
+// console.log((0.1 * 10 + 0.2 * 10) / 10 === 0.3); // true
+
+// Math
+
+// Math.floor(1.7)  // 1
+// Math.ceil(1.7)   // 2
+// Math.round(1.5)  // 2
+// Math.round(1.2)  // 1
+// Math.max(20, 10, 50, 40);   // 50
+// Math.min(20, 10, 50, 40);   // 10
+// Math.pow(2, 4); // 16
+// Math.random() // random in range 0 and 1
+// Math.random()*(10 - 1) + 1 // random in range 1 and 10
+
+// String
+
+// let str = "hello";
+// let phrase = `hi ${str}`;
+
+// const masege = "jave" + "script" + "is" + "awsome":
+
+// result
+
+// result = 5 + "5"
+// console.log(result)  // 55
+
+// result2 = 5 + "5" + 5
+// console.log(result2) // 555
+
+// result3 = 5 + 5 + "5"
+// console.log(result3) // 105
+
+// message =  "Welcome To Bahamas!"
+// console.log(message.lenght);
+
+// console.log(message.toLowerCase())
+// console.log(message.toUpperCase())
+
+// console.log(message.indexOf("T"))   // 8
+// console.log(message.indexOf("heloo"))   // -1
+
+// console.log(message.includes("welcom")) // false
+// console.log(message.includes("Welcom")) // true
+
+// BigInt
+
+// const num = 98765456789876567843456789n;
+// console.log(typeof num);
+
+// let obj = {
+//     "citi": "kuyw",
+//     "age": 500,
+//     "good": true
+// }
+
+// console.log(obj);
+
+// console.log(obj.citi);
+
+// delete obj.age;
+// obj.good = true;
+// console.log(obj[good]);
+
+// obj.age = 45;
+
+// for (key in obj) {
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+
+// let codes = {
+//     49: 'dutch',
+//     44: 'switherland',
+//     41: 'uk',
+//     1: 'usa',
+// };
+
+// for (key in codes) {
+//     console.log(key);    //
+// }
+
+// home work
+
+// let name = "ivan";
+// let city = "kyiv";
+// city = name
+
+// console.log(city)
+
+// let name = olga .'
+// console.log(`hi${1}`)   // hi 1
+// console.log(`hi${name}`)    // hi olga
+// console.log(`hi${"name"}`)  // hi name
+
+// console.log(Number.parseFloat(c))
+// console.log(Number.parseInt(b))
+// console.log(Number.parseInt(a))
+
---
+++ learning_logs/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from .models import Topic
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "learning_logs/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "leaning_logs/topic.html", context)
